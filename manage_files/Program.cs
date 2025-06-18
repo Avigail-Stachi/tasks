dd60eacc79c7eed684dbefa5304dc8aa29f56e06
Avigail_Stachi <a0527102693@gmail.com>
2025-06-18 15:38:57 +0300

    improve without sorting

--- manage_files/Program.cs
+++ manage_files/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using manage_files;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 class Program
@@ -14,7 +15,7 @@ class Program
         string parquetPath = Path.Combine(dataDir, "time_series.parquet");
         try
         {
-            Run1(dataDir,N);
+            var listOfErrorsCommuns= Run1(dataDir,N);
            // Run2(csvPath, parquetPath);
         }
         catch (Exception ex)
@@ -22,21 +23,37 @@ class Program
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-    static void Run1(string dataDir,int N)
+    static List<(string errorCode, int count)> Run1(string dataDir, int N)
     {
         //split_and_count_errors.ReadOne();
 
         int numForThreads = Environment.ProcessorCount;
-        List<string> filesNames= SplitAndCountErrors.MakeDirs(dataDir, numForThreads);
+        List<string> filesNames = SplitAndCountErrors.MakeDirs(dataDir, numForThreads);
 
-        var errorsCount= SplitAndCountErrors.CountErrorsAndMergeWithTPL(filesNames);
-        Console.WriteLine($"count of {Math.Min(N,errorsCount.Count)} errors:");
-        foreach(var e in errorsCount.OrderByDescending(x=>x.Value).Take(N))
-        {
-            Console.WriteLine($"{e.Key}: {e.Value}");
-        }
+        var errorsCount = SplitAndCountErrors.CountErrorsAndMergeWithTPL(filesNames);
         Console.WriteLine($"in total there is: {errorsCount.Count} errors");
+        Console.WriteLine($"count of {Math.Min(N, errorsCount.Count)} errors:");
+        var listOfErrors = new List<(string errorCode, int count)>();
 
+        if (N >= errorsCount.Count)
+        {
+            listOfErrors= errorsCount.Select(x => (x.Key, x.Value)).ToList();
+            foreach (var e in listOfErrors)
+            {
+                Console.WriteLine($"{e.errorCode}: {e.count}");
+            }
+            return listOfErrors;
+
+        }
+        for(int i = 0; i < N; i++)
+        {
+            var max = errorsCount.Aggregate((a, b) => a.Value > b.Value ? a : b);
+            listOfErrors.Add((max.Key,max.Value));
+            errorsCount.TryRemove(max.Key,out _);
+            Console.WriteLine($"{max.Key}: {max.Value}");
+        }
+
+        return listOfErrors;
 
 
     }
